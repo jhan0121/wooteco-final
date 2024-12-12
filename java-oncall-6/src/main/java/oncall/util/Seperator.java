f6a2ca0e23b9249b0f7b2bebdf4178e068d229cb
jhan0121 <jhan0121@gmail.com>
2024-12-12 20:52:54 +0900

    feat(util): seperator 구현

---
+++ java-oncall-6/src/main/java/oncall/util/Seperator.java
@@ -0,0 +1,28 @@
+package oncall.util;
+
+import java.util.List;
+import oncall.exception.ExceptionType;
+
+public class Seperator {
+
+    public static List<String> split(String input) {
+        validate(input);
+        String[] target = input.split(",");
+        for (int i = 0; i < target.length; i++) {
+            target[i] = target[i].trim();
+        }
+        return List.of(target);
+    }
+
+    private static void validate(String value) {
+        if (value.trim().isEmpty()) {
+            throw new IllegalArgumentException(ExceptionType.EMPTY_VALUE.getMessage());
+        }
+        if (value.startsWith(",") || value.endsWith(",")) {
+            throw new IllegalArgumentException(ExceptionType.INVALID_INPUT.getMessage());
+        }
+        if (value.contains(",,")) {
+            throw new IllegalArgumentException(ExceptionType.INVALID_INPUT.getMessage());
+        }
+    }
+}
